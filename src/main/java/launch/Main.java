40cb4d47830228e3e530385584d55e6e3c9c2e58
patladj <F8034RhTie435grt345gt>
2017-11-02 09:13:17 +0200

    Fix Servlet Path22

--- src/main/java/launch/Main.java
+++ src/main/java/launch/Main.java
@@ -1,32 +1,25 @@
 package launch;
 
-import org.apache.catalina.core.StandardContext;
 import org.apache.catalina.startup.Tomcat;
-import org.apache.naming.resources.VirtualDirContext;
 
-import java.io.File;
 
 public class Main {
-    
+
     public static String PORT = System.getenv("PORT");
     public static String HOSTNAME = System.getenv("HOSTNAME");
-    
+
     public static void main(String[] args) throws Exception {
         if (PORT==null || PORT.length()==0) PORT="8080";
         if (HOSTNAME==null || HOSTNAME.length()==0) HOSTNAME="8080";
 
-        String webappDirLocation = "src/main/webapp/";
+        String contextPath = "/" ;
+        String appBase = ".";
         Tomcat tomcat = new Tomcat();
-        tomcat.setPort(Integer.parseInt(PORT));
-
-        StandardContext ctx = (StandardContext) tomcat.addWebapp("/embeddedTomcat",
-                new File(webappDirLocation).getAbsolutePath());
-
-//declare an alternate location for your "WEB-INF/classes" dir:
-        File additionWebInfClasses = new File("target/classes");
-        VirtualDirContext resources = new VirtualDirContext();
-        resources.setExtraResourcePaths("/WEB-INF/classes=" + additionWebInfClasses);
-        ctx.setResources(resources);
+        tomcat.setPort(Integer.valueOf(PORT ));
+        tomcat.setHostname(HOSTNAME);
+        tomcat.getHost().setAppBase(appBase);
+        tomcat.addWebapp(contextPath, appBase);
+//        tomcat.addWebapp(host, url, name, path)
 
         tomcat.start();
         tomcat.getServer().await();
