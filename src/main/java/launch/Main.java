3187400b8ae8b8e2ef193471bc4b129d6f20a59f
patladj <F8034RhTie435grt345gt>
2017-11-02 10:36:57 +0200

    Fix Servlet Path31

--- src/main/java/launch/Main.java
+++ src/main/java/launch/Main.java
@@ -1,11 +1,7 @@
+/* Copyright 2015 Oracle and/or its affiliates. All rights reserved. */
 package launch;
 
-import org.apache.catalina.Context;
 import org.apache.catalina.startup.Tomcat;
-import org.apache.tomcat.util.scan.StandardJarScanner;
-
-import java.io.File;
-
 
 public class Main {
 
@@ -16,39 +12,13 @@ public class Main {
         if (PORT==null || PORT.length()==0) PORT="8080";
         if (HOSTNAME==null || HOSTNAME.length()==0) HOSTNAME="8080";
 
-
-
-        String contextPath = "" ;
-        String appBase = "classes";
+        String contextPath = "/" ;
+        String appBase = ".";
         Tomcat tomcat = new Tomcat();
         tomcat.setPort(Integer.valueOf(PORT ));
         tomcat.setHostname(HOSTNAME);
         tomcat.getHost().setAppBase(appBase);
         tomcat.addWebapp(contextPath, appBase);
-//        tomcat.addWebapp(host, url, name, path)
-
-        tomcat.start();
-        tomcat.getServer().await();
-
-//        String contextPath = "/" ;
-//        String appBase = ".";
-//        Tomcat tomcat = new Tomcat();
-//        tomcat.enableNaming();
-//        tomcat.setPort(Integer.valueOf(PORT ));
-//        tomcat.setHostname(HOSTNAME);
-//        tomcat.getHost().setAppBase(appBase);
-//        tomcat.addWebapp(contextPath, appBase);
-//        String webappDirLocation = "../src/main/webapp/";
-//        Context ctx = tomcat.addWebapp(tomcat.getHost(), "/embeddedTomcat", new File(webappDirLocation).getAbsolutePath());
-//        ((StandardJarScanner) ctx.getJarScanner()).setScanAllDirectories(true);
-//
-//
-//        File additionWebInfClasses = new File("target/classes");
-//        WebResourceRoot resources = new StandardRoot(ctx);
-//        resources.addPreResources(new DirResourceSet(resources, "/WEB-INF/classes", additionWebInfClasses.getAbsolutePath(), "/"));
-//        ctx.setResources(resources);
-
-
 //        tomcat.addWebapp(host, url, name, path)
 
         tomcat.start();
