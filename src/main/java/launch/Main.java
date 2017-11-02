d64afdb5daaebc9fb2c30a083180c1cc5ebc8e70
patladj <F8034RhTie435grt345gt>
2017-11-02 10:12:28 +0200

    Fix Servlet Path27

--- src/main/java/launch/Main.java
+++ src/main/java/launch/Main.java
@@ -19,7 +19,7 @@ public class Main {
 
 
         String contextPath = "/" ;
-        String appBase = "./clssses";
+        String appBase = "clssses";
         Tomcat tomcat = new Tomcat();
         tomcat.setPort(Integer.valueOf(PORT ));
         tomcat.setHostname(HOSTNAME);
